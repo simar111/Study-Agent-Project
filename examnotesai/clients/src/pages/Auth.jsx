575f69bd9626d60f464f55632f9fc86c20fe58d0
Aditi Bishnoi <bishnoi.aditi6@gmail.com>
2026-09-21 15:24:09 +0530

    little changes added

--- examnotesai/clients/src/pages/Auth.jsx
+++ examnotesai/clients/src/pages/Auth.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from "motion/react"
+import { FcGoogle } from "react-icons/fc";
+import { GiFoxTail } from 'react-icons/gi';
 function Auth(){
     return(
         <div className='min-h-screen overflow-hidden bg-white text-black px-8'>
@@ -29,21 +31,55 @@ function Auth(){
            <motion.div
           initial={{opacity: 0 ,x:-60}}
           animate={{opacity:1 , x:0}}
-          transition={{duration:1.5}}>
+          transition={{duration:0.7}}>
           
               <h1 className='text-5xl lg:text-6xl font-extrabold leading-tight
               bg-gradient-to-br from-black/90 via-black/60 to-black/90
               bg-clip-text text-transparent'>
                  Unlock Smart <br/> AI Notes
               </h1>
+                
+                <motion.button 
+                whileHover={{ 
+                    y:-10,
+                    rotateX:8,
+                    rotateY:-8,
+                    scale:1.07
+                 }}
+                 whileTap={{
+                    scale:0.97
+                 }}
+                 transition={{type:"spring",stiffness:200,damping:18}}
+                className='mt-10 px-10 py-3 rounded-xl
+                flex items-center gap-3
+                bg-gradient-to-br from-black/90 via-black/80 to-black/90
+                border border-white/10
+                text-white font-semibold text-lg
+                shadow-[0_25px_60px_rgba(0,0,0,0.7)]'>
+                   
+                    <FcGoogle size={22} />
+                    Continue with Google
+
+                </motion.button>
+
+             <p className="mt-6 max-w-xl text-lg
+             bg-gradient-to-br from-gray-700 via-gray-500/80 to-gray-700
+             bg-clip-text text-transparent">
+                You get <span className="font-semibold">100 FREE</span> to create
+                exam notes, project notes, charts , graphs and 
+                download clean PDFs - instantly using AI.
+             </p>
+             <p className='mt-4 text-sm text-gray-500'>Start with 100 free credits * Upgrade anytime for more credits * 
+                Instant access
+             </p>
 
            </motion.div>
     
           {/*RIGHT CONTENT*/}
 
-            <div>
-
-               
+            <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
+                
+               <Feature icon={}/>
 
             </div>
                 
@@ -52,6 +88,43 @@ function Auth(){
           </main>
 
         </div>
+    )
+}
+
+function Feature({icon , title , des}){
+    return(
+       <motion.div 
+       whileHover={{ 
+                    y:-12,
+                    rotateX:8,
+                    rotateY:-8,
+                    scale:1.05
+                 }}
+        transition={{type:"spring",stiffness:200,damping:18}}         
+       className='relative rounded-2xl p-6
+       bg-gradient-to-br from-black/90 via-black/80 to-black/90
+       backdrop-blur-2xl
+       border border-white/10
+       shadow-[0_30px_80px_rgba(0,0,0,0.7)]
+       text-white'
+       style={{ transformStyle : 'preserve-3d'}}
+       >
+        <div className='absolute inset-0 rounded-2xl
+        bg-gradient-to-br from-white/10 to-transparent
+        opacity-0 hover:opacity-100 transition-opacity
+        pointer-events-none '>
+         <div className='relative z-10' style={{ transform : "translateZ(30px)"}}>
+              <div className='text-4xl mb-3'>
+                 {icon}
+              </div>
+              <h3 className='text-lg font-semibold mb-2'>{title}</h3>
+              <p className='text-gray-300 text-sm leading-relaxed'>{des}</p>
+
+         </div>
+        </div>
+       
+
+       </motion.div>
     )
 }
 
